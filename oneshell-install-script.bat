30ba0933d76b8620cb7233dc732c0b3858e1ee50
Manikanta Reddy Pasala <manikantareddy.pasala@gmail.com>
2024-11-09 11:01:47 +0400

    Update oneshell-install-script.bat

--- oneshell-install-script.bat
+++ oneshell-install-script.bat
@@ -83,6 +83,7 @@ if not exist "%EXE_NAME%" (
         goto :EOF
     )
 )
+
 :: Start Docker Compose and wait for all services to start
 echo Starting Docker Compose...
 docker-compose up -d
@@ -98,7 +99,8 @@ echo Waiting for services to be healthy...
 set "ALL_HEALTHY=true"
 for /f "tokens=*" %%i in ('docker-compose ps -q') do (
     set "CONTAINER=%%i"
-    for /f %%j in ('docker inspect -f "{{.State.Health.Status}}" %%i') do set "STATUS=%%j"
+    set "STATUS="
+    for /f "tokens=*" %%j in ('docker inspect -f "{{.State.Health.Status}}" %CONTAINER%') do set "STATUS=%%j"
     if /i not "%STATUS%"=="healthy" (
         echo Service %%i is not healthy yet...
         set "ALL_HEALTHY=false"
@@ -107,7 +109,7 @@ for /f "tokens=*" %%i in ('docker-compose ps -q') do (
 )
 if /i "%ALL_HEALTHY%"=="false" goto WAIT_LOOP
 
-echo All services are healthy
+echo All services are healthy.
 
 :: Remove existing service if it exists
 %NSSM_PATH% stop "OneShellPrinterUtilService" >nul 2>&1
